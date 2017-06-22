f3aa08ba3a4fc92f31c72b7f4f68171fe47b6e65
Andrew <andrey.albul@gmail.com>
2017-06-23 00:07:41 +0300

    1 SmartGIt

---
+++ Test.php
@@ -0,0 +1,10 @@
+<?php
+$cats = 3;
+
+echo $cats;
+/**
+ * Created by PhpStorm.
+ * User: Андрей
+ * Date: 16.06.2017
+ * Time: 20:51
+ */
---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Accusamus ad adipisci aperiam consequuntur culpa eligendi est facilis fuga ipsa laudantium magni
+        nulla pariatur quaerat, qui quisquam rem repellat sit veniam.</p>
+    <?php
+        $a = 10;
+        $b = $a;
+        echo ($a . "</br>" . $b);
+    ?>
+</body>
+</html>
